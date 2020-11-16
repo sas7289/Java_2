cf0aedc718e6762929cd0c32b452bfbc86d28d06
Alex Shakhov <sas7289@yandex.ru>
2020-11-16 21:09:51 +0300

    changes

--- src/Lesson_2/Main.java
+++ src/Lesson_2/Main.java
@@ -10,9 +10,16 @@ public class Main {
                 {"qwe", "4", "123q", "98"},
                 {"b", "90", "231", "2"},
                 {"q", "2x2", "231", "2"}};
+        String[][] testRight = {{ "23123", "231", "2", "42"},
+                {"1", "4", "123", "98"},
+                {"3", "90", "231", "2"},
+                {"55", "22", "231", "2"}};
+
 
         try {
-            Example.transform(testWrongType);
+//            System.out.println(Example.transform(testWrongSize));
+//            System.out.println(Example.transform(testWrongType));
+            System.out.println(Example.transform(testRight));
         } catch (MyArraySizeException | MyArrayDataException e){
             System.out.println(e.getMessage());
         }
--- src/Lesson_2/MyArrayDataException.java
+++ src/Lesson_2/MyArrayDataException.java
@@ -2,6 +2,6 @@ package Lesson_2;
 
 public class MyArrayDataException extends ClassCastException {
     public MyArrayDataException(int i, int j) {
-        super("Невозможно преобразовать в Integer значения, лежащие в ячейке [" + i + "][" + j + "]");
+        super("Невозможно преобразовать в Integer значения, находящиеся в ячейке [" + i + "][" + j + "]");
     }
 }
